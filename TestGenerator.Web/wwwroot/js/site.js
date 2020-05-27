acd165ee8bf53aa66363e6b78ddcf7e212b4d419
Christopher Walker <christophersvt3g@gmail.com>
2020-05-27 15:31:39 +0200

    Début d'ajout jQuery pour la page de création de question

--- TestGenerator.Web/wwwroot/js/site.js
+++ TestGenerator.Web/wwwroot/js/site.js
@@ -2,3 +2,8 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(function () {
+    $('#questionTypeSelect').on('change', function () {
+        alert($('#questionTypeSelect').val());
+    });
+});
