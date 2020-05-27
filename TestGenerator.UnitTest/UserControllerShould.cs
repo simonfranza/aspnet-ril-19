e3591ec8213dc007d0fa69e6af8752e7d6744df0
Timothée Simon-Franza <timothee.simonfranza@viacesi.fr>
2020-05-27 20:39:47 +0200

    Ajout de la logique de connexion.

--- TestGenerator.UnitTest/UserControllerShould.cs
+++ TestGenerator.UnitTest/UserControllerShould.cs
@@ -7,38 +7,44 @@ using Moq;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using TestGenerator.Model.Data;
 using TestGenerator.Model.Entities;
 using TestGenerator.Web.Controllers;
 using TestGenerator.Web.Models;
 using Xunit;
+using SignInResult = Microsoft.AspNetCore.Identity.SignInResult;
 
 namespace TestGenerator.UnitTest
 {
     public class UserControllerShould
     {
-        public TestGeneratorContext GetFakeContext()
+        public Mock<SignInManager<User>> GetFakeSignInManager(Mock<UserManager<User>> mockedUserManager)
         {
-            var options = new DbContextOptionsBuilder<TestGeneratorContext>()
-                .UseInMemoryDatabase("FakeDatabase")
-                .Options;
-
-            var context = new TestGeneratorContext(options);
-
-            var fixture = new Fixture();
-            fixture.Behaviors.OfType<ThrowingRecursionBehavior>().ToList()
-                .ForEach(b => fixture.Behaviors.Remove(b));
-            fixture.Behaviors.Add(new OmitOnRecursionBehavior());
-
-            context.Users.Add(fixture.Create<User>());
-            context.Users.Add(fixture.Create<User>());
-            context.Users.Add(fixture.Create<User>());
-            context.SaveChanges();
-
-            return context;
+            return new Mock<SignInManager<User>>(
+                mockedUserManager.Object,
+                new HttpContextAccessor(),
+                new Mock<IUserClaimsPrincipalFactory<User>>().Object,
+                new Mock<IOptions<IdentityOptions>>().Object,
+                new Mock<ILogger<SignInManager<User>>>().Object,
+                new Mock<IAuthenticationSchemeProvider>().Object
+            );
         }
 
+        public Mock<IHttpContextAccessor> GetFakeHttpContextAccessor()
+        {
+            var mockHttpContextAccessor = new Mock<IHttpContextAccessor>();
+            var context = new DefaultHttpContext();
+            var fakeTenantId = "abcd";
+            context.Request.Headers["Tenant-ID"] = fakeTenantId;
+            mockHttpContextAccessor.Setup(_ => _.HttpContext).Returns(context);
+
+            return mockHttpContextAccessor;
+        }
 
         [Fact]
         public async Task Register_Returns_Bad_Request_Result_When_ModelState_Is_Invalid()
@@ -46,8 +52,11 @@ namespace TestGenerator.UnitTest
             // Arrange
             var userStoreMock = Mock.Of<IUserStore<User>>();
             var userMgrMock = new Mock<UserManager<User>>(userStoreMock, null, null, null, null, null, null, null, null);
+            var signinMgrMock = GetFakeSignInManager(userMgrMock);
+            
+            
 
-            var controller = new UserController(GetFakeContext(), userMgrMock.Object);
+            var controller = new UserController(userMgrMock.Object, signinMgrMock.Object, GetFakeHttpContextAccessor().Object);
             controller.ModelState.AddModelError("Email", "Required");
             var userViewModel = new UserRegistrationViewModel();
 
@@ -64,11 +73,10 @@ namespace TestGenerator.UnitTest
         {
             // Arrange
             var fixture = new Fixture();
-            var context = GetFakeContext();
-
             var userStoreMock = Mock.Of<IUserStore<User>>();
-
             var userMgrMock = new Mock<UserManager<User>>(userStoreMock, null, null, null, null, null, null, null, null);
+            var signinMgrMock = GetFakeSignInManager(userMgrMock);
+
             userMgrMock
                 .Setup(mgr => mgr.CreateAsync(It.IsNotNull<User>(), It.IsAny<string>()))
                 .ReturnsAsync(IdentityResult.Success)
@@ -84,7 +92,7 @@ namespace TestGenerator.UnitTest
                 .ReturnsAsync(IdentityResult.Success)
                 .Verifiable();
 
-            var controller = new UserController(context, userMgrMock.Object);
+            var controller = new UserController(userMgrMock.Object, signinMgrMock.Object, GetFakeHttpContextAccessor().Object);
 
             // Act
             var result = await controller.Register(fixture.Create<UserRegistrationViewModel>());
@@ -99,10 +107,11 @@ namespace TestGenerator.UnitTest
         {
             // Arrange
             var fixture = new Fixture();
-            var context = GetFakeContext();
 
             var userStoreMock = Mock.Of<IUserStore<User>>();
             var userMgrMock = new Mock<UserManager<User>>(userStoreMock, null, null, null, null, null, null, null, null);
+            var signinMgrMock = GetFakeSignInManager(userMgrMock);
+
             userMgrMock
                 .Setup(mgr => mgr.CreateAsync(It.IsNotNull<User>(), It.IsAny<string>()))
                 .ReturnsAsync(IdentityResult.Success)
@@ -118,7 +127,7 @@ namespace TestGenerator.UnitTest
                 .ReturnsAsync(IdentityResult.Success)
                 .Verifiable();
 
-            var controller = new UserController(context, userMgrMock.Object);
+            var controller = new UserController(userMgrMock.Object, signinMgrMock.Object, GetFakeHttpContextAccessor().Object);
             var userViewModel = fixture.Create<UserRegistrationViewModel>();
             userViewModel.Email = "test@cesi.fr";
             
@@ -138,10 +147,10 @@ namespace TestGenerator.UnitTest
         {
             // Arrange
             var fixture = new Fixture();
-            var context = GetFakeContext();
-
             var userStoreMock = Mock.Of<IUserStore<User>>();
             var userMgrMock = new Mock<UserManager<User>>(userStoreMock, null, null, null, null, null, null, null, null);
+            var signinMgrMock = GetFakeSignInManager(userMgrMock);
+
             userMgrMock
                 .Setup(mgr => mgr.CreateAsync(It.IsNotNull<User>(), It.IsAny<string>()))
                 .ReturnsAsync(IdentityResult.Success)
@@ -157,7 +166,7 @@ namespace TestGenerator.UnitTest
                 .ReturnsAsync(IdentityResult.Success)
                 .Verifiable();
 
-            var controller = new UserController(context, userMgrMock.Object);
+            var controller = new UserController(userMgrMock.Object, signinMgrMock.Object, GetFakeHttpContextAccessor().Object);
             var userViewModel = fixture.Create<UserRegistrationViewModel>();
             userViewModel.Email = "test@viacesi.fr";
             
@@ -170,6 +179,85 @@ namespace TestGenerator.UnitTest
             userMgrMock.Verify(mgr => mgr.CreateAsync(It.IsNotNull<User>(), It.IsAny<string>()), Times.Once);
             userMgrMock.Verify(mgr => mgr.AddToRoleAsync(It.IsAny<User>(), "Admin"), Times.Never);
             userMgrMock.Verify(mgr => mgr.AddToRoleAsync(It.IsAny<User>(), "User"), Times.Once);
+        }
+
+        [Fact]
+        public async Task Login_Should_Display_Error_Message_When_ModelState_Is_Invalid()
+        {
+            // Arrange
+            var userStoreMock = Mock.Of<IUserStore<User>>();
+            var userMgrMock = new Mock<UserManager<User>>(userStoreMock, null, null, null, null, null, null, null, null);
+            var signinMgrMock = GetFakeSignInManager(userMgrMock);
+
+            var controller = new UserController(userMgrMock.Object, signinMgrMock.Object, GetFakeHttpContextAccessor().Object);
+            controller.ModelState.AddModelError("Email", "Required");
+            var userViewModel = new UserLoginViewModel();
+
+            // Act
+            await controller.Login(userViewModel);
+
+            // Assert
+            var modelState = controller.ModelState;
+            Assert.Contains("Global", modelState.Keys);
+            Assert.True(modelState["Global"].Errors.Count > 0);
+            Assert.Equal("Identifiants erronés.", modelState["Global"].Errors[0].ErrorMessage);
+        }
+
+        [Fact]
+        public async Task Login_Should_Display_Error_Message_When_Bad_Credentials_Inputted()
+        {
+            // Arrange
+            var fixture = new Fixture();
+            var userStoreMock = Mock.Of<IUserStore<User>>();
+            var userMgrMock = new Mock<UserManager<User>>(userStoreMock, null, null, null, null, null, null, null, null);
+            var signinMgrMock = GetFakeSignInManager(userMgrMock);
+
+            signinMgrMock
+                .Setup(mgr => mgr.PasswordSignInAsync(It.IsNotNull<string>(), It.IsNotNull<string>(), It.IsAny<bool>(), It.IsAny<bool>()))
+                .ReturnsAsync(SignInResult.Failed)
+                .Verifiable();
+
+            var controller = new UserController(userMgrMock.Object, signinMgrMock.Object, GetFakeHttpContextAccessor().Object);
+
+            // Act
+            await controller.Login(fixture.Create<UserLoginViewModel>());
+
+            // Assert
+            var modelState = controller.ModelState;
+            Assert.Contains("Global", modelState.Keys);
+            Assert.True(modelState["Global"].Errors.Count > 0);
+            Assert.Equal("Identifiants erronés.", modelState["Global"].Errors[0].ErrorMessage);
+        }
+
+        [Fact]
+        public async Task Login_Should_Redirect_To_Home_Index_On_Success()
+        {
+            // Arrange
+            var fixture = new Fixture();
+            var userStoreMock = Mock.Of<IUserStore<User>>();
+            var userMgrMock = new Mock<UserManager<User>>(userStoreMock, null, null, null, null, null, null, null, null);
+            var signinMgrMock = GetFakeSignInManager(userMgrMock);
+
+            userMgrMock
+                .Setup(mgr => mgr.FindByEmailAsync(It.IsNotNull<string>()))
+                .ReturnsAsync(fixture.Create<User>())
+                .Verifiable();
+
+            signinMgrMock
+                .Setup(mgr => mgr.PasswordSignInAsync(It.IsNotNull<string>(), It.IsNotNull<string>(), It.IsAny<bool>(), It.IsAny<bool>()))
+                .ReturnsAsync(SignInResult.Success)
+                .Verifiable();
+
+            var controller = new UserController(userMgrMock.Object, signinMgrMock.Object, GetFakeHttpContextAccessor().Object);
+
+            // Act
+            var result = await controller.Login(fixture.Create<UserLoginViewModel>()) as RedirectToActionResult;
+
+            // Assert
+            signinMgrMock.Verify(mgr => mgr.PasswordSignInAsync(It.IsNotNull<string>(), It.IsNotNull<string>(), It.IsAny<bool>(), It.IsAny<bool>()), Times.Once);
+            Assert.NotNull(result);
+            Assert.Equal("Home", result.ControllerName);
+            Assert.Equal("Index", result.ActionName);
         }
     }
 }
