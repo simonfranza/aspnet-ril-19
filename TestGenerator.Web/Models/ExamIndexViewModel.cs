797e9d1eb13541574f74b68e0b2562a48473f078
Hynel <timothee.simonfranza@viacesi.fr>
2020-05-29 23:12:05 +0200

    Ajout de la possibilité de filtrer les examens par module (#14)

---
+++ TestGenerator.Web/Models/ExamIndexViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestGenerator.Model.Entities;
+
+namespace TestGenerator.Web.Models
+{
+    public class ExamIndexViewModel
+    {
+        public ICollection<Exam> Exams { get; set; }
+
+        public ICollection<Module> Modules { get; set; } = new List<Module>();
+
+        public string ModuleId = "";
+    }
+}
