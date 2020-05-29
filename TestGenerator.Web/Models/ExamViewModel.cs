e27bf4111e5666537e8269d4671678752ecc8f3c
Hynel <timothee.simonfranza@viacesi.fr>
2020-05-29 23:57:09 +0200

    Modification de la carte d'examen (#17)

---
+++ TestGenerator.Web/Models/ExamViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using TestGenerator.Model.Entities;
+
+namespace TestGenerator.Web.Models
+{
+    public class ExamViewModel
+    {
+        public int ExamId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public int QuestionAmount { get; set; }
+
+        public int AuthorizedAttempts { get; set; }
+
+        public int Duration { get; set; }
+
+        public DateTime ClosingDate { get; set; }
+
+        public Module Module { get; set; }
+
+        public ICollection<ExamQuestion> Questions { get; set; }
+
+        public ICollection<Model.Entities.ExamAttempt> UserExamAttempts { get; set; }
+    }
+}
