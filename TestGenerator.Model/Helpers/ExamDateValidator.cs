b815d41c7f84e54f1f8ab31b738e29cf636d9e39
Christopher Walker <christophersvt3g@gmail.com>
2020-05-29 17:48:55 +0200

    Ajout d'un validateur perso pour la date de cloture

---
+++ TestGenerator.Model/Helpers/ExamDateValidator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace TestGenerator.Model.Helpers
+{
+    public class ExamDateValidatorAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            DateTime dt = (DateTime)value;
+            if (dt >= DateTime.UtcNow)
+            {
+                return ValidationResult.Success;
+            }
+
+            return new ValidationResult(ErrorMessage ?? "La date de clôture doit être supérieure à la date du jour.");
+        }
+
+    }
+}
